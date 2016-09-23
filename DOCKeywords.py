b9a5c83f58df0b98d2edf9c3e994725f461e69f8
mastermentis <sayantand@Sayantans-MacBook-Air.local>
2016-09-23 20:20:34 +0530

    Added comments

--- DOCKeywords.py
+++ DOCKeywords.py
@@ -9,6 +9,9 @@ from sets import Set
 from cStringIO import StringIO
 from Keywords import Keywords
 
+############################################################################################
+# This class generates and stores the keywords read from a
+# microsoft docx file. 
 
 class DOCKeywords(Keywords):
 
@@ -16,6 +19,7 @@ class DOCKeywords(Keywords):
         self._keys = []
         self._fname = file_name
 
+    # Generate Keywords from docx file _fname
     def generateKeywords(self):
         doc = docx.Document(self._fname)
         for para in doc.paragraphs:
